0637fcb731e86b2da5996ffdfb0502756c6a4733
piglei <piglei2007@gmail.com>
2014-06-16 14:50:25 +0800

    Fix package data

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 setup(name='uwsgi-sloth',
-    version='1.0.1',
+    version='1.0.2',
     description='A simple uwsgi access log analyzer',
     long_description=open('README.rst').read(),
     author='piglei',
@@ -12,9 +12,7 @@ setup(name='uwsgi-sloth',
     keywords='uwsgi log analyzer',
     license='LICENSE',
     packages=['uwsgi_sloth'],
-    data_files=[
-      ('uwsgi_sloth', ['uwsgi_sloth/report.html', ])
-    ],
+    package_data={"": ['*.html']},
     install_requires=[
         'jinja2',
     ],

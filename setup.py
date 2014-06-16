59f014240416c2db284eff908973cd6feb140536
piglei <piglei2007@gmail.com>
2014-06-16 14:24:27 +0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
-from distutils.core import setup
+from setuptools import setup
 
-def readme():
-    with open('README.rst') as f:
-        return f.read()
 
 setup(name='uwsgi-sloth',
-      version='1.0.0',
-      description='A simple uwsgi access log analyzer',
-      long_description=readme(),
-      author='piglei',
-      author_email='piglei2007@gmail.com',
-      url='https://github.com/piglei/uwsgi-sloth',
-      keywords='uwsgi log analyzer',
-      license='LICENSE',
-      packages=['uwsgi_sloth'],
-      install_requires=[
-          'jinja2',
-      ],
-      scripts=['uwsgi_sloth/uwsgi-sloth'])
+    version='1.0.0',
+    description='A simple uwsgi access log analyzer',
+    long_description=open('README.rst').read(),
+    author='piglei',
+    author_email='piglei2007@gmail.com',
+    url='https://github.com/piglei/uwsgi-sloth',
+    keywords='uwsgi log analyzer',
+    license='LICENSE',
+    packages=['uwsgi_sloth'],
+    data_files=[
+      ('uwsgi_sloth', ['uwsgi_sloth/report.html', ])
+    ],
+    install_requires=[
+        'jinja2',
+    ],
+    scripts=['uwsgi_sloth/uwsgi-sloth'])
 
 

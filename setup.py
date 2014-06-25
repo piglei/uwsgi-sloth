5fed1b64f101c89b94909f69958f9986966b8cc3
piglei <piglei2007@gmail.com>
2014-06-26 01:22:45 +0800

    Fix setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name='uwsgi-sloth',
-    version='1.0.2',
+    version='2.0.0',
     description='A simple uwsgi access log analyzer',
     long_description=open('README.rst').read(),
     author='piglei',
@@ -11,8 +11,8 @@ setup(name='uwsgi-sloth',
     url='https://github.com/piglei/uwsgi-sloth',
     keywords='uwsgi log analyzer',
     license='LICENSE',
-    packages=['uwsgi_sloth'],
-    package_data={"": ['*.html']},
+    packages=find_packages(),
+    package_data={"": ['templates/*.html', 'sample.conf']},
     install_requires=[
         'jinja2',
         'configobj'
